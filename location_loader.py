1237a48c91f9b8b099ceff136c75cc602972f3d4
Brittany S <57652157+bsteenbergen@users.noreply.github.com>
2023-10-24 10:51:55 -0700

    Working on loader files

--- location_loader.py
+++ location_loader.py
@@ -7,17 +7,15 @@ SOURCE = 'GlobalLandTemperaturesByCity.csv'
 with open(SOURCE, 'r+', encoding='utf-8') as f:
     reader = csv.reader(f)
     next(reader)
-    count = 0
-    while count < 5:
-        for row in reader:
-            if '' in row:
-                continue
-            else:
-                city = row[3]
-                country = row[4]
-                latitude = row[5]
-                longitude = row[6]
-            print(f'INSERT INTO location VALUES(\'{city}\', \'{country}\', \'{latitude}\', \'{longitude}\');')
-    count += 1
+    for row in reader:
+        if '' in row:
+            continue
+        else:
+            city = row[3]
+            country = row[4]
+            latitude = row[5]
+            longitude = row[6]
+
+        print(f'INSERT INTO location VALUES(\'{city}\', \'{country}\', \'{latitude}\', \'{longitude}\');')
 
 print('COMMIT;')
