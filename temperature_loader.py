92722e7f19a2a89cfe4f3de7480206c7218635f9
Brittany S <57652157+bsteenbergen@users.noreply.github.com>
2023-11-06 09:49:29 -0800

    temperature loader working

--- temperature_loader.py
+++ temperature_loader.py
@@ -1,18 +1,35 @@
 import csv
+import re
 
 print('BEGIN;')
 
 SOURCE = 'GlobalLandTemperaturesByCity.csv'
+
+locations = set()
 with open(SOURCE, 'r+', encoding='utf-8') as f:
     reader = csv.reader(f)
     next(reader)
+
+    rows = []
     for row in reader:
-        id = row[0]
-        if row[1] == '' or row[2] == '':
+        rows.append(tuple(row))
+    
+    list(set([r for r in rows]))
+
+    for row in rows:
+        if '' in row:
             continue
         else:
+            city = row[3].replace('\'', ' ')
+            country = row[4].replace('\'', ' ')
+            date_collected = row[0]
             avg_temp = row[1]
             avg_temp_uncertainty = row[2]
-        print(f'INSERT INTO temperature VALUES(\'{id}\', {avg_temp}, {avg_temp_uncertainty});')
+            location_key = (city, country)
+            if location_key in locations:
+                continue
+            else:
+                locations.add(location_key)
+                print(f'INSERT INTO temperature VALUES(\'{city}\', \'{country}\', \'{date_collected}\', \'{avg_temp}\', \'{avg_temp_uncertainty}\');')
 
 print('COMMIT;')
