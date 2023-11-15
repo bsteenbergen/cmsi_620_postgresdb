4c6be555a4a36467cb4db5ddf4a9eec9ec9910d3
Brittany S <57652157+bsteenbergen@users.noreply.github.com>
2023-11-15 15:26:49 -0800

    added sea level to schema

--- schema.sql
+++ schema.sql
@@ -1,9 +1,9 @@
--- SCHEMA: schema
+-- -- SCHEMA: schema
 
--- DROP SCHEMA IF EXISTS schema ;
+-- -- DROP SCHEMA IF EXISTS schema ;
 
-CREATE SCHEMA IF NOT EXISTS schema
-    AUTHORIZATION postgres;
+-- CREATE SCHEMA IF NOT EXISTS schema
+--     AUTHORIZATION postgres;
 	
 CREATE TABLE location (
   city VARCHAR,
@@ -20,4 +20,24 @@ CREATE TABLE temperature (
   avg_temp FLOAT,
   avg_temp_uncertainty FLOAT,
   FOREIGN KEY (city, country) REFERENCES location
+);
+
+CREATE TABLE country_sea (
+    country VARCHAR,
+    sea VARCHAR
+);
+
+CREATE TABLE mean_sea_level(
+    object_id INT PRIMARY KEY,
+    country VARCHAR,
+    iso2 VARCHAR,
+    iso3 VARCHAR,
+    indicator VARCHAR,
+    unit VARCHAR,
+    source VARCHAR,
+    cts_code VARCHAR,
+    cts_name VARCHAR,
+    measure VARCHAR,
+    date DATE,
+    value FLOAT
 );
