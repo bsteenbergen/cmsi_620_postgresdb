d68b5ef5c985c4cf8d0e9cf61917b56daca8f59d
Brittany S <57652157+bsteenbergen@users.noreply.github.com>
2023-11-20 14:42:45 -0800

    fixed loaders so that null values were included

--- emissions_loader.py
+++ emissions_loader.py
@@ -9,11 +9,14 @@ with open(SOURCE, 'r+', encoding='utf-8') as f:
     next(reader)
 
     for row in reader:
-        country = row[0].replace('\'', ' ')
-        year_measured = row[1]
-        population = row[3]
-        co2 = row[7]
-        cumulative_co2 = row[25]
-        print(f'INSERT INTO emissions (country, year_measured, population, co2, cumulative_co2) VALUES(\'{country}\', \'{year_measured}\', \'{population}\', \'{co2}\', \'{cumulative_co2}\');')
+        if '' in row:
+            continue
+        else:
+            country = row[0].replace('\'', ' ')
+            year_measured = row[1]
+            population = row[3]
+            co2 = row[7]
+            cumulative_co2 = row[25]
+            print(f'INSERT INTO emissions (country, year_measured, population, co2, cumulative_co2) VALUES(\'{country}\', \'{year_measured}\', NULLIF(\'{population}\', \'\')::bigint, NULLIF(\'{co2}\', \'\')::float, NULLIF(\'{cumulative_co2}\', \'\')::float);')
 
 print('COMMIT;')
--- temperature_loader.py
+++ temperature_loader.py
@@ -14,6 +14,6 @@ with open(SOURCE, 'r+', encoding='utf-8') as f:
         date_collected = row[0]
         avg_temp = row[1]
         avg_temp_uncertainty = row[2]
-        print(f'INSERT INTO temperature (city, country, date_collected, avg_temp, avg_temp_uncertainty) VALUES(\'{city}\', \'{country}\', \'{date_collected}\', \'{avg_temp}\', \'{avg_temp_uncertainty}\');')
+        print(f'INSERT INTO temperature (city, country, date_collected, avg_temp, avg_temp_uncertainty) VALUES(\'{city}\', \'{country}\', \'{date_collected}\', NULLIF(\'{avg_temp}\', \'\')::float, NULLIF(\'{avg_temp_uncertainty}\', \'\')::float);')
 
 print('COMMIT;')
